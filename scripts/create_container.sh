4158661e22549e06ffd5fff70c91eb0cb53527c6
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-11-08 15:39:25 +0000

    Changed scripts to wait for solr to initialize

--- scripts/create_container.sh
+++ scripts/create_container.sh
@@ -6,12 +6,11 @@ docker rm gamegalaxy
 # Create the docker container with the cores
 docker run -p 8983:8983 --name gamegalaxy -v data -d solr:9.3 solr-precreate games
 
+# Wait for Solr to initialize
+sleep 20
+
 # Insert schema
-curl -X POST -H 'Content-type:application/json'  \
---data-binary "@./schema1.json"  \
-http://localhost:8983/solr/games/schema
+curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/schema1.json" http://localhost:8983/solr/games/schema
 
 # Populate
-curl -X POST -H 'Content-type:application/json' \
---data-binary "@./games_collection.json" \ 
-http://localhost:8983/solr/games/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection.json" http://localhost:8983/solr/games/update?commit=true
--- scripts/manage_core.sh
+++ scripts/manage_core.sh
@@ -2,12 +2,11 @@ docker exec gamegalaxy bin/solr delete -c games
 
 docker exec gamegalaxy bin/solr create_core -c games
 
+# Wait for Solr to initialize
+sleep 20
+
 # Insert schema
-curl -X POST -H 'Content-type:application/json'  \
---data-binary "@./schema1.json"  \
-http://localhost:8983/solr/games/schema
+curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/schema1.json" http://localhost:8983/solr/games/schema
 
 # Populate
-curl -X POST -H 'Content-type:application/json' \
---data-binary "@./games_collection.json" \ 
-http://localhost:8983/solr/games/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection.json" http://localhost:8983/solr/games/update?commit=true
