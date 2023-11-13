4b4519640fead6ff49c66af418ed54767f02d591
Barbarafilipa <barbarafilipacarvalho08@gmail.com>
2023-11-13 08:39:26 +0000

    Retrieval com schema 2

--- scripts/create_container.sh
+++ scripts/create_container.sh
@@ -15,14 +15,7 @@ docker cp "$PWD"/search-engine/stopwords.txt gamegalaxy:/var/solr/data/games/con
 docker cp "$PWD"/search-engine/synonyms.txt gamegalaxy:/var/solr/data/games/conf
 
 # Insert schema
-curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/schema1.json" http://localhost:8983/solr/games/schema
+curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/schema2.json" http://localhost:8983/solr/games/schema
 
 # Populate
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection.json" http://localhost:8983/solr/games/update?commit=true
-
-# Remove automatically created field
-curl -X POST -H 'Content-type: application/json' --data '{
-  "delete-field": {
-    "name": "_nest_path_"
-  }
-}' http://localhost:8983/solr/games/schema
