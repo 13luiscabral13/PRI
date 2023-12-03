1a260f2a80149ab13f6557dd0f2d2deea67509b0
Barbarafilipa <barbarafilipacarvalho08@gmail.com>
2023-12-03 17:42:01 +0000

    URL para o solr adicionado

--- my-app/src/components/ResultsPage.js
+++ my-app/src/components/ResultsPage.js
@@ -3,12 +3,38 @@
 import React from 'react';
 import { useLocation, Routes, Route } from 'react-router-dom';
 import GameCard from './GameCard';
-import GamePage from './GamePage';
 
 const ResultsPage = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const searchTerm = searchParams.get('search');
+  const searchText = searchParams.get('search');
+
+  console.log("Search text: ", searchText);
+
+  const processQuery = async () => {
+    const editedSearchText = searchText.replaceAll(" ", '%20');
+
+    const domain = "http://localhost:8983/solr/games/select?";
+    const op = "%20OR%20";
+    const bq = "bq=%7B!child%20of%3D%22*%3A*%20-_nest_path_%3A*%22%7D";
+    const bqName = "name%3A(" + editedSearchText + ")%5E2";
+    const bqSummary = "summary%3A(" + editedSearchText + ")";
+    const bqWikipedia = "wikipedia%3A(" + editedSearchText + ")%5E2";
+    const bqGenre = "genre%3A(" + editedSearchText + ")%5E3";
+    const others = "&defType=edismax&fl=*%2C%5Bchild%5D&fq=%7B!child%20of%3D%22*%3A*%20-_nest_path_%3A*%22%7Dname%3A*&indent=true&q.op=OR&";
+    const q = "q=(" + editedSearchText + ")&";
+    const qf = "qf=platform%5E8%20review%5E2";
+    const extra = "&rows=1000&useParams=&wt=json";
+
+    const link = domain+bq+bqName+op+bqSummary+op+bqWikipedia+op+bqGenre+others+q+qf+extra;
+    console.log("URL: ", link);
+
+    let results_reviews = await fetch(link, { mode: 'cors' });
+
+    console.log("Reviews: ", results_reviews);
+  }
+
+  processQuery();
 
   // Mock data for demonstration purposes
   const searchResults = [
@@ -30,26 +56,18 @@ const ResultsPage = () => {
   return (
     <div>
       <h2>Search Results</h2>
-      <p>Showing results for: {searchTerm}</p>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              {searchResults.map((result) => (
-                <GameCard
-                  key={result.id}
-                  id={result.id}
-                  title={result.title}
-                  summary={result.summary}
-                  releaseDate={result.releaseDate}
-                />
-              ))}
-            </div>
-          }
-        />
-        <Route path="/game/:gameId" element={<GamePage />} />
-      </Routes>
+      <p>Showing results for: {searchText}</p>
+        <div>
+          {searchResults.map((result) => (
+            <GameCard
+              key={result.id}
+              id={result.id}
+              title={result.title}
+              summary={result.summary}
+              releaseDate={result.releaseDate}
+            />
+          ))}
+        </div>
     </div>
   );
 };
