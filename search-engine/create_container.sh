54c36353b6ee756d6c8202d488cd850373ceff16
Isabel Amaral <isabel.andre.amaral@gmail.com>
2023-12-09 23:13:39 +0000

    Added term vector component

--- search-engine/create_container.sh
+++ search-engine/create_container.sh
@@ -12,18 +12,17 @@ sleep 20
 # Import stop words file
  docker cp "$PWD"/search-engine/stopwords.txt gamegalaxy:/var/solr/data/games/conf
 # Import synonyms file
- docker cp "$PWD"/search-engine/synonyms.txt gamegalaxy:/var/solr/data/games/conf
+docker cp "$PWD"/search-engine/synonyms.txt gamegalaxy:/var/solr/data/games/conf
+# Import config
+docker cp "$PWD"/search-engine/solrconfig.xml gamegalaxy:/var/solr/data/games/conf
 
 # Insert schema
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/improved_schema.json" http://localhost:8983/solr/games/schema
 
 # Populate
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection_part1.json" http://localhost:8983/solr/games/update?commit=true
-
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection_part2.json" http://localhost:8983/solr/games/update?commit=true
-
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection_part3.json" http://localhost:8983/solr/games/update?commit=true
-
 curl -X POST -H 'Content-type:application/json' --data-binary "@./search-engine/games_collection_part4.json" http://localhost:8983/solr/games/update?commit=true
 
 # Add more like this handler
